356c035e2fad5c84f1914a5c6d5d41928252c590
Wilson <sancheswilson29@gmail.com>
2020-05-02 13:50:33 +0100

    final commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,6 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
     const transactions = await this.find();
 
     const { income, outcome } = transactions.reduce(
